ba72f577abc50db178ec4282bc670fd3f49699ab
W3rty5U <filipm.gda@gmail.com>
2021-12-19 21:17:48 +0100

    structy do przechowywania danych

--- Sekretariat/MainWindow.xaml.cs
+++ Sekretariat/MainWindow.xaml.cs
@@ -24,5 +24,43 @@ namespace Sekretariat
         {
             InitializeComponent();
         }
+
+        struct Osoba
+        {
+            string imie;
+            string drugieImie;
+            string nazwisko;
+            string nazwiskoPanienskie;
+            string pesel;
+            string zdjecie;
+            char plec;
+            string imieMatki;
+            string imieOjca;
+            DateTime dataUrodzenia;
+        }
+
+        struct Uczen
+        {
+            Osoba osoba;
+            string klasa;
+            List<string> grupy;
+        }
+
+        struct Nauczyciel
+        {
+            Osoba osoba;
+            string wychowawstwo;
+            List<string> przedmioty;
+            List<Tuple<string, string>> nauczanie;
+            DateTime dataZatrudnienia;
+        }
+
+        struct pracownikObslugi
+        {
+            Osoba osoba;
+            string etat;
+            string opis;
+            DateTime dataZatrudnienia;
+        }
     }
 }
